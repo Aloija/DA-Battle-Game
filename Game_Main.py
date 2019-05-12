5e1b5c172d4015cf8b302d375581795b6ac77073
Aloija <Aloijaarel@gmail.com>
2019-05-12 19:11:03 +0300

    test

--- Game_Main.py
+++ Game_Main.py
@@ -16,7 +16,7 @@ while is_game != 0:
         print('\nХодит', character.get_name())
         game.actions(character)
         for effect in character.buffs:
-            character.effect_reduce_duration(effect)
+            character.effect_reduce_duration(effect)    # МОЖНО ОБЪЕДЕНИТЬ
             character.remove_effect(effect)
         for effect in character.debuffs:
             character.effect_reduce_duration(effect)
